07a3f85ece68c16a085940187189a1175b5e8110
Michael <crossovver@outlook.com>
2016-02-01 00:37:27 -0500

    sdsfdsf Signed-off-by: Michael <crossovver@outlook.com>

---
+++ MySQL LoginForm_JFX/src/projectform/MySqlConnect.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectform;
+import java.sql.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Michael
+ */
+public class MySqlConnect {
+    Connection conn=null;
+    public static Connection ConnectDB(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://kingsmanscode.com:3306/mtechnik_mybb","mtechnik_phpn1", "January291981");
+            JOptionPane.showMessageDialog(null,"Connected to Database");
+            return conn;
+        }
+    catch(Exception e){
+        JOptionPane.showMessageDialog(null, e);
+        return null;
+        
+}
+        }
+    }
+
+         
+
